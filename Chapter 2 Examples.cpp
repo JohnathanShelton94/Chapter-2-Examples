618dd8721f7b0bc89d81f622663d1bf2803370ff
Johnathan Shelton <js24634@email.vccs.edu>
2024-09-08 17:52:19 -0400

    Added Example 2 Code

--- Chapter 2 Examples.cpp
+++ Chapter 2 Examples.cpp
@@ -1,10 +1,16 @@
-// MyConsoleApp.cpp : This file contains the Chapter 2 Examples Project
-//
+//This program calculates and displays the discounted value of an item.
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Chapter 2 Examples\n";
+	double regularPrice, percentDiscount, discountAmount,
+		reducedPrice;
+	regularPrice = 59.95;
+	percentDiscount = 0.2;
+	discountAmount = regularPrice * percentDiscount;
+	reducedPrice = regularPrice - discountAmount;
+	cout << "The discount amount is $" << discountAmount << endl;
+	cout << "The discounted price is $" << reducedPrice;
+	return 0;
 }
-
